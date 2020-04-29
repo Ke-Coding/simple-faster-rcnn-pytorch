37c07d060f73f5799f252d62d0a52674f65c7d52
tky_hm_xps13 <k_code@163.com>
2020-04-29 18:54:20 +0800

    plot

--- utils/config.py
+++ utils/config.py
@@ -25,7 +25,7 @@ class Config:
     # 0.0005 in origin paper but 0.0001 in tf-faster-rcnn
     weight_decay = 0.0005
     # lr_decay = 0.1  # 1e-3 -> 1e-4
-    warm_up_lr = 0.001
+    warm_up_lr = 0.0004
     lr = warm_up_lr / 4
     warm_up_iter = 350
     warm_up_delta = (warm_up_lr - lr) / warm_up_iter
